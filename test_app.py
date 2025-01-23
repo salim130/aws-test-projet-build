306fb3ab3b06120686de4b155540a8010679ec3c
salim <salim.wat13@gmail.com>
2025-01-23 12:07:19 +0100

    feat:implemmentation de la feat hello

---
+++ test_app.py
@@ -0,0 +1,13 @@
+import unittest
+from app import say_hello
+
+
+
+class TestApp(unittest.TestCase):
+    def test_say_hello(self):
+        self.assertEqual(say_hello("AWS"), "hello,AWS")
+
+
+
+if __name__ == "__main__":
+    unittest.main()
